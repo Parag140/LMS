7d3b25539e3af3a9d7fc5894a9e5fade123b9ef0
parag140 <guptaparag1401@gmail.com>
2025-09-11 01:06:49 +0530

    vercel json added

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schems({
+const userSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
