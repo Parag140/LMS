94c6eaff3e02464b37e92713f209c6958f713130
parag140 <guptaparag1401@gmail.com>
2025-09-14 12:22:40 +0530

    changed 6 files

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
@@ -11,19 +12,32 @@ export const AppContextprovider = (props) => {
   const [isEducator, setIsEducator] = useState(true);
   const [enrolledCourses, setEnrolledCourses] = useState([]);
   const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const { user } = useUser();
 
   //fetch all course
   const fetchAllCourses = async () => {
     setAllCourses(dummyCourses);
   };
   //fetch user enrolled courses
-  const fetchUserEnrolledCourses = async()=>{
+  const fetchUserEnrolledCourses = async () => {
     setEnrolledCourses(dummyCourses);
-  }
+  };
   useEffect(() => {
     fetchAllCourses();
     fetchUserEnrolledCourses();
   }, []);
+
+  const logToken = async () => {
+    console.log(await getToken());
+  };
+
+  useEffect(() => {
+    if (user) {
+      logToken();
+    }
+  }, [user]);
+
   //function to calculate average rating of course
   const calculateRating = (course) => {
     if (course.courseRatings.length == 0) return 0;
@@ -48,15 +62,15 @@ export const AppContextprovider = (props) => {
     return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
   };
   //function to calculate number of lectures in a course
- const calculateNoOfLectures = (course) => {
-  let totalLectures = 0;
-  course.courseContent.forEach((chapter) => {
-    if (Array.isArray(chapter.chapterContent)) {
-      totalLectures += chapter.chapterContent.length;
-    }
-  });
-  return totalLectures;
-};
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
 
   const value = {
     currency,
@@ -69,7 +83,7 @@ export const AppContextprovider = (props) => {
     calculateCourseDuration,
     calculateNoOfLectures,
     enrolledCourses,
-    fetchUserEnrolledCourses
+    fetchUserEnrolledCourses,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
--- server/server.js
+++ server/server.js
@@ -3,17 +3,23 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
 
 const app = express();
 
 await connectDB();
+await connectCloudinary();
 
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get("/", (req, res) => {
   res.send("Hello World from Express!");
 });
 app.post("/clerk", express.json(), clerkWebhooks);
+app.use('/api/educator',express.json(),educatorRouter)
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
